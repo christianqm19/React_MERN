aa50aab009883bebd4ec2279e9cbae30b130b1df
christianqm19 <christian.qm.19@gmail.com>
2024-02-08 16:56:11 -0500

    Commit Video 5

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
- 
+
+//settings
+app.set('port', process.env.PORT || 4000);
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+//app.get('/api/users', (req, res) => res.send('Users Routes'));
+//app.get('/api/notes', (req, res) => res.send('Notes Routes'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/notes', require('./routes/notes'));
+
 module.exports = app;
+
+//MONGODB_URI = mongodb://localhost/mernstack
---
+++ backend/src/database.js
@@ -0,0 +1,22 @@
+//Requerimos el modulo: mongoose
+const mongoose = require('mongoose');
+
+//Creamos la BD: mernstack
+const URI = process.env.MONGODB_URI 
+    ? process.env.MONGODB_URI
+    : 'mongodb://localhost/databasetest';
+
+mongoose.connect(URI
+    //,{
+    //useNewUrlParser: true,
+    //useCreateIndex: true
+    //}
+);
+
+//Guardamos la conexion en una constante
+const connection = mongoose.connection;
+
+//Mostramos un mensaje si existe la conexion
+connection.once('open', () => {
+    console.log('Se realizó la conexión con la BD..');
+});
